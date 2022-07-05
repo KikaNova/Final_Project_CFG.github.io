6df598c860813554af7c446216c44bcd5d07eac2
KikaNova <105739060+KikaNova@users.noreply.github.com>
2022-07-05 18:05:54 +0100

    First attempts at coding. Bio webpage.
    2 page website with some css and javascript.

---
+++ project/myScript1.js
@@ -0,0 +1,8 @@
+function increment() {
+    document.getElementById("count").innerText++;
+}
+document.addEventListener('DOMContentLoaded', function() {
+    const counter = document.getElementById("count");
+    counter.innerText = 0;
+    
+})
